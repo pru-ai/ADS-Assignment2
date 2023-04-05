4abb1393b4c9d27539d60bc63c1f4cc98e28f13c
Pruthvi <pruthvisai.24@gmail.com>
2023-04-05 18:07:44 +0100

    Stats code merge

--- ADS-Assignment2.py
+++ ADS-Assignment2.py
@@ -1,22 +1,22 @@
+import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
 import seaborn as sns
+import stats
+import scipy.stats as sc
 
 
 def read_csv(filename):
     """
-    Reads data from a CSV file containing World Bank data on renewable energy
-    consumption as a percentage of total final energy consumption
-    for all countries.
+    Read CSV file and extract dataframe as per country and year with
+    indexes configures as required.
 
     Parameters:
-    filename (str): The name of the CSV file containing the data.
+    filename (String): Name of the CSV file
 
     Returns:
-    tuple: A tuple containing two pandas DataFrames. The first DataFrame is a
-    year-based DataFrame with each column representing a country and each row
-    representing a year. The second DataFrame is a country-based DataFrame with
-    each row representing a country and each column representing a year.
+    Two Dataframes containing Year wise data and Country wise data
+    respectively.
     """
     df = pd.read_csv(filename, skiprows=4)
     df.drop(columns = ['Country Code', 'Indicator Code', 'Unnamed: 66'],
@@ -53,8 +53,49 @@ df_compare_brazil_features = df_brazil[["CO2 emissions from liquid fuel"
                                         r" value added (% of GDP)",
                                         "Foreign direct investment,"
                                         r" net inflows (% of GDP)"]]
+
+# Statistics
+df_compare_brazil_features.describe()
+
+br_low, br_high = stats.bootstrap(df_brazil["Urban population"
+                                            " (% of total population)"],
+                                  np.mean)
+
+in_low, in_high = stats.bootstrap(df_india["Urban population"
+                                           " (% of total population)"],
+                                  np.mean)
+
+us_low, us_high = stats.bootstrap(df_usa["Urban population"
+                                         " (% of total population)"],
+                                  np.mean)
+
+sa_low, sa_high = stats.bootstrap(df_sa["Urban population"
+                                        " (% of total population)"],
+                                  np.mean)
+
+country_urban_pop_interval = {'Brazil': (br_low, br_high),
+                              'India': (in_low, in_high),
+                              'USA': (us_low, us_high),
+                              'South Africa': (sa_low, sa_high)}
+
+print(country_urban_pop_interval)
+
 corr_matrix = df_compare_brazil_features.corr()
 plt.figure(figsize = (8, 5))
 ax = sns.heatmap(corr_matrix, annot = True)
 ax.set_title('Brazil', fontdict = {'fontsize': 12}, pad = 12)
 ax.set(xlabel = "", ylabel = "")
+
+indicator = "Population in urban agglomerations of more than 1 million (% of total population)"
+countries = ['Brazil', 'India', 'United States', 'South Africa']
+urban_agglo = year_data[year_data['Indicator Name'].isin([indicator])]
+
+urban_agglo = urban_agglo[urban_agglo['Country Name'].isin(countries)]
+urban_agglo_t = urban_agglo.T
+
+#urban_agglo.plot(x='Country Name',
+##        kind='bar',
+#        stacked=False,
+#        title='Urban Population')
+
+
