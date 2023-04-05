825a90f02ecf215a146633211636e8e70a6ce379
Pruthvi <pruthvisai.24@gmail.com>
2023-04-05 21:26:59 +0100

    Final commit including docstring and comments

--- ADS-Assignment2.py
+++ ADS-Assignment2.py
@@ -6,6 +6,7 @@ import stats
 from matplotlib.ticker import FuncFormatter
 
 
+# Function to read WorldBank data into year wise and country wise dataframes
 def read_csv(filename):
     """
     Read CSV file and extract dataframe as per country and year with
@@ -29,7 +30,23 @@ def read_csv(filename):
 year_data, country_data = read_csv('WorldBank.csv')
 
 
+# Segregate countries dataframe as per the required country
 def country_wise_data(countryname):
+    """
+    Slice the main dataframe country wise and return the features
+    associated to that particular country
+
+    Parameters
+    ----------
+    countryname : String
+        Name of the country.
+
+    Returns
+    -------
+    df : dataframe
+        Returns country related data in a data frame.
+
+    """
     df = country_data[countryname]
     df.columns = df.iloc[0]
     df = df.iloc[1:]
@@ -44,12 +61,13 @@ df_india = country_wise_data("India")
 df_usa = country_wise_data("United States")
 df_sa = country_wise_data("South Africa")
 
-# Statistics
+# Applying statistical methods each country wise
 df_brazil.describe()
 df_india.describe()
 df_usa.describe()
 df_sa.describe()
 
+# Creating a dict of Urban population intervals of the four countries
 br_low, br_high = stats.bootstrap(df_brazil["Urban population"
                                             " (% of total population)"],
                                   np.mean)
@@ -71,9 +89,10 @@ country_urban_pop_interval = {'Brazil': (br_low, br_high),
                               'USA': (us_low, us_high),
                               'South Africa': (sa_low, sa_high)}
 
-print(country_urban_pop_interval)
+print("Urban Population % Interval at 90% confidence for each country",
+      country_urban_pop_interval)
 
-
+# Choosing indicators to be displayed in the heatmap
 indicators = ["CO2 emissions from liquid fuel consumption (kt)",
               "Urban population (% of total population)",
               "Agricultural land (sq. km)",
@@ -81,7 +100,17 @@ indicators = ["CO2 emissions from liquid fuel consumption (kt)",
               "Foreign direct investment, net inflows (% of GDP)"]
 
 
+# To generate a heatmap based on the correlation matrix between the indicators
 def heat_map(country):
+    """
+    Generate heatmap for the features associated to a specific country
+
+    Parameters
+    ----------
+    country : String
+        Name of the country.
+
+    """
     df_features = country_wise_data(country)[indicators]
     corr_matrix = df_features.corr()
     plt.figure(figsize = (8, 5))
@@ -96,15 +125,37 @@ heat_map("India")
 heat_map("South Africa")
 
 
-cntrys = ['Brazil', 'India', 'United States', 'South Africa']
+# Function to convert large numbers into millions notation for plot
+def convert_to_millions(num, pos):
+    """
+    Convert a given large number into millions notation
 
+    Parameters
+    ----------
+    num : Integer
+        DESCRIPTION.
 
-def convert_to_millions(num):
+    Returns
+    -------
+    String
+        Return the number converted to millions notation.
+
+    """
     'The two args are the value and tick position'
     return '%1.1fM' % (num * 1e-6)
 
 
+# Function to generate time series plots for a particular indicator
 def time_series(indicator):
+    """
+    Generate time series plot for a specific indicator of a country
+
+    Parameters
+    ----------
+    indicator : String
+        Name of the feature.
+    """
+    cntrys = ['Brazil', 'India', 'United States', 'South Africa']
     df_agg = year_data[year_data['Indicator Name'].isin([indicator])]
     df_agg = df_agg[df_agg['Country Name'].isin(cntrys)].reset_index(drop=True)
     df_agg_t = df_agg.T
